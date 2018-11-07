0f36efc8d10e09f7a696e7780b823e894ff5e7b6
wangyafei <3218566389@qq.com>
2018-11-07 14:48:57 +0800

    服务器连接数据库完毕

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import koa from 'koa'
+import cors from "koa-cors";
+import bodyParser from 'koa-bodyparser'
+
+// 打印日志
+import logger from 'koa-logger'
+
+// 路由文件
+import router from "./router/index";
+
+import query from './uitls/query'
+
 const app =new koa()
 
-
+app
+    .use(logger())
+    .use(cors())
+    .use(bodyParser())
+    .use(router.routes())
 
 app.listen(3000,()=>{
     console.log('node is ok')
